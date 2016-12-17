2b79bce13d3789a7e6ca9baa1492ed05278d57a7
Ryan Cloono <rcloono@meddbase.com>
2016-12-17 16:19:15 +0000

    Initial Commit

---
+++ SwiftPlay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftPlay
+//
+//  Created by Ryan Cloono on 17/12/2016.
+//  Copyright © 2016 Ryan Cloono. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
